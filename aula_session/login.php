da49ce824d407d989dc3c592c7712e7ef63061a4
Rodrigo Britto Calovi <rodrigo-calovi@uergs.edu.br>
2020-07-19 14:51:53 -0300

    aula cookies

--- aula_session/login.php
+++ aula_session/login.php
@@ -1,8 +1,20 @@
 <?php
 session_start();
-define('PASS', '827ccb0eea8a706c4c34a16891f84e7b');
+define('PASS', '827ccb0eea8a706c4c34a16891f84e7b'); //12345
+define('NAME', 'Rodrigo Calovi');
 
 $senha = filter_input(INPUT_POST, 'txtSenha', FILTER_SANITIZE_STRING);
+
+if(PASS==md5($senha)){
+    $_SESSION['logado']=1;
+    setcookie('nome',NAME,time()+3600,'/');
+
+    header('Location: painel.php');
+} else {
+    echo 'Acesso-negado!';
+
+    header('Location: negado.php');
+}
 
 
 //echo md5($senha);
---
+++ aula_session/negado.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+    <head>
+        <title>NEGADO!</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <section>
+            <h1>Acesso Negado</h1>
+            <p>Você não pode acessar este painel!</p>
+            <a href="index.php">Voltar</a>
+        </section>
+    </body>
+
+</html>
